804346308871ccec481b3e91577c5ee66fd3d994
hackandcode <hackandcode@web.de>
2021-10-07 08:41:49 +0200

    added Register Component

--- src/Register.js
+++ src/Register.js
@@ -1,10 +1,22 @@
 import { Row, Card, Form, Button } from 'react-bootstrap';
 
-function Login() {
+function Register() {
     return (
         <Card style={{ width: '18rem' }}>
             <Card.Body>
                 <Form>
+                    <Form.Group className="mb-3" controlId="formBasicFirst">
+                        <Form.Label>Firstname</Form.Label>
+                        <Form.Control type="text" placeholder="Firstname" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicLast">
+                        <Form.Label>Lastname</Form.Label>
+                        <Form.Control type="text" placeholder="Lastname" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicUser">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="text" placeholder="Username" />
+                    </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
                         <Form.Control type="email" placeholder="Enter email" />
@@ -12,16 +24,14 @@ function Login() {
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="remember me" />
+                        <Form.Control type="password" placeholder="repeat Password" />
                     </Form.Group>
                     <Row>
                     <Button variant="primary" type="submit" className="col-6">
-                        Login
+                        Register
                     </Button>
                     <Form.Label className="col-6">
-                        <a href="/register">Register</a>
+                        <a href="/login">Login</a>
                     </Form.Label>
                     </Row>
                     </Form>
@@ -29,4 +39,4 @@ function Login() {
             </Card>);
 }
 
-export default Login;
+export default Register;
